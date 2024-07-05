aba77990a361d074a69e893702ab5e7ef9cad4ca
repirisyan <repirisyan@icloud.com>
2024-07-05 15:14:44 +0700

    dev scheduler

--- routes/console.php
+++ routes/console.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('app:daily-clear-crawling')->weeklyOn(7,'00:00');
-Schedule::command('app:daily-crawling')->weeklyOn(7,'01:00');
-Schedule::command('app:index-crawler')->weeklyOn(1,'00:00');
+// Schedule::command('app:daily-clear-crawling')->weeklyOn(7,'00:00');
+// Schedule::command('app:daily-crawling')->weeklyOn(7,'01:00');
+// Schedule::command('app:index-crawler')->weeklyOn(1,'00:00');
+
+Schedule::command('app:daily-clear-crawling');
+Schedule::command('app:daily-crawling');
+Schedule::command('app:index-crawler');
 
 
