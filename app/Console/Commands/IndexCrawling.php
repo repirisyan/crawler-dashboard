b1391f1d80937560f26cd1db2d3268818b7c2412
repirisyan <repirisyan@icloud.com>
2024-07-08 14:34:56 +0700

    add indexing command

---
+++ app/Console/Commands/IndexCrawling.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class IndexCrawling extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:index-crawling';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Indexing Data Crawling';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+                $request = new \stdClass();
+                $request->marketplace = 'indexing';
+                Http::post(env('APP_CRAWLER_API').'/crawler', $request);
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Facades\Schedule;
 // Schedule::command('app:daily-clear-crawling')->weeklyOn(7,'00:00');
 // Schedule::command('app:daily-crawling')->weeklyOn(7,'01:00');
 
-Schedule::command('app:daily-clear-crawling');
-Schedule::command('app:daily-crawling');
+// Schedule::command('app:daily-clear-crawling');
+// Schedule::command('app:daily-crawling');
+Schedule::command('app:index-crawling');
 
 
